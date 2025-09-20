c98efaedf878dd8a9d60b81fecdd1dc1466f352b
alan-m-shen <52120925+alan-m-shen@users.noreply.github.com>
2025-09-20 04:54:22 -0400

    Initial Commit

---
+++ Notion Swift/Notion_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notion_SwiftApp.swift
+//  Notion Swift
+//
+//  Created by Alan on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct Notion_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
